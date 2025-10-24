37f38c6addefd6ce97140c644c4a8dd2faa6ca5c
Will B Chang <will.b.chang@gmail.com>
2025-10-24 17:12:57 +0800

    test: update Money to use float for amounts and add division test

--- go/money_test.go
+++ go/money_test.go
@@ -9,7 +9,7 @@ func TestMultiplication(t *testing.T) {
 	}
 	tenner := fiver.Times(2)
 	if tenner.amount != 10 {
-		t.Errorf("Expected 10, got [%d]", tenner.amount)
+		t.Errorf("Expected 10, got [%f]", tenner.amount)
 	}
 }
 
@@ -20,15 +20,30 @@ func TestMultiplicationInEuro(t *testing.T) {
 	}
 	result := money.Times(2)
 	if result.amount != 20 {
-		t.Errorf("Expected 20, got[%d]", result.amount)
+		t.Errorf("Expected 20, got[%f]", result.amount)
+	}
+}
+
+func TestDivisionKRW(t *testing.T) {
+	money := Money{
+		amount:   4002,
+		currency: "KRW",
+	}
+	result := money.Divides(4)
+	if result.amount != 1000.5 {
+		t.Errorf("Expected 1000.5, got [%f]", result.amount)
 	}
 }
 
 type Money struct {
-	amount   int
+	amount   float32
 	currency string
 }
 
-func (d Money) Times(n int) Money {
-	return Money{amount: d.amount * n, currency: d.currency}
+func (m Money) Times(n float32) Money {
+	return Money{amount: m.amount * n, currency: m.currency}
+}
+
+func (m Money) Divides(n float32) Money {
+	return Money{amount: m.amount / n, currency: m.currency}
 }
